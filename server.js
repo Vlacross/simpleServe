c53fa42dba9051d343cd2980a4fccab46e2e2499
Tim Vlasuk <timvlasuk@gmail.com>
2019-05-05 22:55:49 -0700

    setting up simple server

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const cors = require('cors');
+
+
+app.use(cors())
+app.use(jsonParser)
+
+
+
+app.get('/', (req, res) => {
+  console.log('simple get success')
+  res.send("Thanks for waking me up...")
+})
+
+app.post('/merch-side-product', jsonParser, (req, res) => {
+    let missingFields
+    const requiredFields = ['uuid', 'subscription', 'custName', 'fiscalToken'];
+    missingFields = requiredFields.filter(field => !req.body[field])
+    if(missingFields.length > 0) {
+        return res.json({
+            type: 'error',
+            code: 451,
+            message: `Missing ${missingFields} in request headers, cannot process`
+        })
+    }
+    return res.json({
+      success: true,
+      code: 200,
+      message: 'merchant product transaction completed successfully'})
+})
+
+
+
+
+
+
+
+
+app.all('/*', (req, res) => {
+res.send("Sorry, no end-point detected!")})
+
+
+app.listen(8080)
