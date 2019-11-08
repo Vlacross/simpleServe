732fac51c0f0eb6a84cb56d9ef1e03382f84b631
Tim Vlasuk <timvlasuk@gmail.com>
2019-11-07 23:01:51 -0800

    set up simple wrapper function that includes access control allow origin headers for any site that need it

--- utils.js
+++ utils.js
@@ -1,7 +1,20 @@
+const request = require('request');
+
 const emailCheck = val => (/^[A-Za-z\d\.\-]+@+[A-Za-z\d\.\-]+(\.)+[A-Za-z\d\.\-]+$/g.test(val) ? undefined : 'Incorrect email format!')
 
+function alarm_clock(url) {
+  let opts = {
+    url: url,
+    headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+  }
+  request(opts);
+}
 
 module.exports = {
-  emailCheck
+  emailCheck,
+  alarm_clock
 }
 
